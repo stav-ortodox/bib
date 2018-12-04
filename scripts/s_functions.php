3d1d963466e7aa7542da8d4015eca715fb257a86
stav-ortodox <stav-ortodox@mail.ru>
2018-12-04 17:49:02 +0300

    новый сайдбар

--- scripts/s_functions.php
+++ scripts/s_functions.php
@@ -26,11 +26,12 @@ function get_header_site ($title_br, $header_subtitle) {
 	<link href="/css/mdb.min.css" rel="stylesheet">
 
 	<!-- Your custom styles (optional) -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.2.3/foundation.min.css">
 	<link href="/css/style.css" rel="stylesheet">
 	<link rel='stylesheet' href='/css/bootstrap-grid.min.css'>
 
 	<!-- My js -->
-	<link rel="stylesheet" href="/js/my_js.js">
+	<script src="/js/sticky.min.js"></script>
 
 	<!-- Fonts -->
 	<link href="http://allfont.ru/allfont.css?fonts=ds-russia-demo" rel="stylesheet" type="text/css">
@@ -81,6 +82,16 @@ function get_header_site ($title_br, $header_subtitle) {
 		});
 		});
 	</script>
+
+	<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-30914018-3', 'auto');
+      ga('send', 'pageview');
+  </script>
 </head>
 
 <body>
@@ -242,7 +253,9 @@ echo <<<EOD
 </footer>
 </body>
 </html>
-
+<script>
+  var sticky = new Sticky('[data-sticky]');
+</script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
