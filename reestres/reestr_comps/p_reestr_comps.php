ececa878120dce0738e9ff3d1960ef73190609f5
stav-ortodox <45417701+stav-ortodox@users.noreply.github.com>
2018-12-26 01:21:17 +0300

    Дом 26.12 1:20
    начал разработку стилей печати

--- reestres/reestr_comps/p_reestr_comps.php
+++ reestres/reestr_comps/p_reestr_comps.php
@@ -10,8 +10,45 @@ get_menu ();
 
 echo "<button type='button' class='button1 btn aqua-gradient' id='printPageButton'><a href='form_reestr_comps.php'>Вызвать форму</a></button>";
 
-table_reestr_comp('Реестр компьютеров и оргтехники храма св. Великомученика и Целителя Пантелеимона г. Ставрополя');
+table_reestr_comp('Реестр компьютеров и оргтехники храма св. Великомученика и Целителя Пантелеимона г. Ставрополя');?>
 
+
+
+	<div class="container reestr-comps-parrent">
+			<img class="img-thumbnail img-fluid z-depth-3" src="uploads/lables/DSC_0218.JPG" alt="храм святого Великомученика и Целителя Пантелеимона г. Ставрополя">
+	</div>
+
+
+<div class="copyright text-center">
+	<span>Издательский центр храма св. Великомученика и Целителя Пантелеимона</span> <br>
+	<small>Ставрополь 2018 г.</small>
+</div>
+
+<!-- ************************************************************************************************* -->
+
+<div class="house_html container reestr-comps-parrent">
+	<h1><?php table_reestr_comp('Административный корпус') ?></h1>
+	<div class="house_html_img">
+		<img class="img-thumbnail img-fluid z-depth-3" src="uploads/lables/DSC_0218.JPG" alt="">
+	</div>
+</div>
+
+<!-- ************************************************************************************************* -->
+<div class='table'>
+	<table>
+		<tr>
+			<th class='font-RGIA'>№</th>
+			<th class='font-RGIA'>Наименование</th>
+			<th class='font-RGIA'>марка/модель</th>
+			<th class='font-RGIA'>Технические характеристики</th>
+			<th class='font-RGIA'>Место нахождения</th>
+			<th class='font-RGIA'>Ответственное лицо</th>
+			<th class='font-RGIA'>Состояние</th>
+			<th class='font-RGIA'>Фотография</th>
+		</tr>
+
+
+<?php
 $select_query = 
 sprintf("
 	SELECT * FROM reestr_comps, house, place 
@@ -55,8 +92,8 @@ sprintf("SELECT COUNT(1) FROM reestr_comps");
 $result = mysqli_query($link, $select_query);
 $row = mysqli_fetch_array($result);
 $count = $row[0] - 1;	
-
 ?>
+
 <div><p>Всего количество техники: <?php echo $count?></p></div>
 <?php
 
