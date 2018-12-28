6f2fc7487e15313dd68b7d40d8801f2de27463e1
stav-ortodox <stav-ortodox@mail.ru>
2018-12-28 16:48:04 +0300

    купав

--- reestres/reestr_comps/p_reestr_comps.php
+++ reestres/reestr_comps/p_reestr_comps.php
@@ -227,8 +227,7 @@ echo "</table>
 
 
 <?php
-$select_query = 
-sprintf("
+$select_query = sprintf("
 	SELECT * FROM reestr_comps, house, place 
 	WHERE reestr_comps.house = 'Храм' and reestr_comps.house = house.house and reestr_comps.place = place.place
 	ORDER BY house.house_id, place.place_id, reestr_comps.name");
@@ -243,7 +242,55 @@ echo "
 <td>".$row[""]."</td>"."\n"."
 <td>".$row["name"]."</td>"."\n"."
 <td>".$row["name_type"]."</td>"."\n"."
-<td>".preg_replace("/[\r\n]+/", "</p><p>", $row['characterics'])."</td>"."\n"."
+<td>".$row['characterics']."</td>"."\n"."
+<td>".$row["place"]."</td>"."\n"."	
+<td>".$row["of_face"]."</td>"."\n"."
+<td>".$row["sost"]."</td>"."\n"."
+<td>
+	<div class='box'>
+		<img class='img-thumbnail' src=".$row["image"]." alt=''>
+	</div> 
+	<div id='printPageButton'>
+	<a href= p_edit_reestr_comps.php?id=".$row["id"]."><i class='fa fa-edit fa-2x green-text' aria-hidden='true' title='Редактировать'></i></a>"."\n"."
+	<a href= delete_reestr_comps.php?id=".$row["id"]."><i class='fa fa-times-circle-o fa-2x red-text' aria-hidden='true' title='Удалить'></i></a>
+	</div>
+</td>"."\n"."
+</tr>\n";
+}
+echo "</table>
+</div>
+";?>
+
+<div class='table'>
+	<table>
+		<tr>
+			<th class='font-RGIA'>№</th>
+			<th class='font-RGIA'>Наименование</th>
+			<th class='font-RGIA'>марка/модель</th>
+			<th class='font-RGIA'>Технические характеристики</th>
+			<th class='font-RGIA'>Место нахождения</th>
+			<th class='font-RGIA'>Ответственное лицо</th>
+			<th class='font-RGIA'>Состояние</th>
+			<th class='font-RGIA'>Фотография</th>
+		</tr>
+
+<?php
+$select_query = sprintf("
+	SELECT * FROM reestr_comps, house, place 
+	WHERE reestr_comps.house = 'Трапезная' and reestr_comps.house = house.house and reestr_comps.place = place.place
+	ORDER BY house.house_id, place.place_id, reestr_comps.name");
+
+
+$result = mysqli_query($link, $select_query);
+while ($row = mysqli_fetch_array($result))
+
+{ // выводим данные
+echo "
+<tr>\n
+<td>".$row[""]."</td>"."\n"."
+<td>".$row["name"]."</td>"."\n"."
+<td>".$row["name_type"]."</td>"."\n"."
+<td>".$row['characterics']."</td>"."\n"."
 <td>".$row["place"]."</td>"."\n"."	
 <td>".$row["of_face"]."</td>"."\n"."
 <td>".$row["sost"]."</td>"."\n"."
@@ -261,8 +308,6 @@ echo "
 echo "</table>
 </div>
 ";
-
-
 
 
 // $select_query = 
