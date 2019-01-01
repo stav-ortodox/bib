4caea81f8841a765dbd6cd972d68aab7b8ea9bfd
stav-ortodox <stav-ortodox@mail.ru>
2019-01-01 18:49:50 +0300

    кабинет 01.01 18:46

--- raspisanie/index.php
+++ raspisanie/index.php
@@ -3,17 +3,18 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="../css/bootstrap.min.css">
-	<link href="https://fonts.googleapis.com/css?family=Old+Standard+TT" rel="stylesheet">
-	<link rel="stylesheet" href="https://unpkg.com/hartija---css-print-framework@1.0.0/print.css" type="text/css" media="print" charset="utf-8">
-</head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+	<!-- <link rel="stylesheet" href="https://unpkg.com/gutenberg-css@0.4" media="print"> -->
 <body>
+<html>
 
-	<page size="A4" class="A4">
+<div class="format">
+
+	<div class="wrap">
 		<div class="A4-border"><img src="image/frame-2487087_960_720.png" alt=""></div>
 		<div class="opacity"><img src="image/thumb_l_0689.jpg" alt=""></div>
-		
+
 		<div class="header">
 			<img src="../images/original-233x285.png" alt="">
 			<div class="title">
@@ -28,60 +29,272 @@
 		<div class="content">
 			<table class="table-r">
 				<tr>
-					<td rowspan="2" class="td-min-width">Вторник<br>1 января</td>
-					<td style="color: red;">7:30</td>
-					<td><p><h6>Божественная Литургия. Исповедь. 
-						Молебен с Акафистом Донской иконе Божьей Матери и водоосвящнением.
-					Заупокойная панихида.</h6></p>
-					<p style="color: #54749C;">служащий священник:</p>
-					<p style="color: #7030A0;">сослужащий(-е) священник(-и):</p>
-					<p style="color: #943634;">помогающий священник:</p> 
-					<p style="color: #35A907;">отдыхающий священник:</p>
-				</td>
-			</tr>
-			<tr>
-				<td style="color: red;">17:00</td>
-				<td>
-					<p><h6>Вечернее Богослужение. Исповедь. Полиелей свт. Алексию, митр. Киевскому, Московскому и всея России чудотворцу
-					Заупокойная лития.</p></h6>
-					<p style="color: #54749C;">служащий священник:</p>
-					<p style="color: #7030A0;">сослужащий(-е) священник(-и):</p>
-					<p style="color: #943634;">помогающий священник:</p> 
-					<p style="color: #35A907;">отдыхающий священник:</p>
-				</td>
-			</tr>
-		</table>
-		<table class="table-r">
+					<td rowspan="4" class="td-min-width">Вторник<br>1 января</td>
+					<td style="color: red;"><h3>0:00</h3></td>
+
+					<td>
+						<br>
+						<p><h3 class="bold"><u>Гражданский Новый год.</u><br>
+							Божественная Литургия.
+							Исповедь.
+						Молебен на начало Нового года.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="color: red;"><h3>8:00</h3></td>
+					<td>
+						<p><h3 class="bold">Божественная Литургия.
+							Исповедь.
+							Молебен с чтением Акафиста
+							мч. Вонифатию и
+							водоосвящением. Заупокойная
+						панихида.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="color: red;"><h3>12:00</h3></td>
+					<td>
+						<p><h3 class="bold">Таинство Соборования.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="color: red;"><h3>17:00</h3></td>
+					<td>
+						<p><h3 class="bold">Вечернее Богослужение.
+							Исповедь.
+							Полиелей св. прав. Иоанну
+							Кронштадскому. Заупокойная
+						лития.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+			</table>
+		</div>		
+	</div>
+</div>
+
+<!-- СРЕДА 2 ЯНВАРЯ -->
+
+<div class="format">
+	<div class="wrap">
+		<div class="A4-border"><img src="image/frame-2487087_960_720.png" alt=""></div>
+		<div class="opacity"><img src="image/thumb_l_0689.jpg" alt=""></div>
+
+		<div class="header">
+			<img src="../images/original-233x285.png" alt="">
+			<div class="title">
+				<h3 class="text-center">
+					БОГОСЛУЖЕНИЯ В ХРАМЕ СВЯТОГО
+					ВЕЛИКОМУЧЕНИКА И ЦЕЛИТЕЛЯ
+					ПАНТЕЛЕИМОНА
+				</h3>
+				<h4 class="text-center">http://stav-panteleimon.ru</h4>
+			</div>
+		</div>
+		<!-- СРЕДА 2 ЯНВАРЯ -->
+		<div class="content">
+			<table class="table-r">
 				<tr>
 					<td rowspan="2" class="td-min-width">Среда<br>2 января</td>
-					<td style="color: red;">7:30</td>
-					<td><p><h6>Божественная Литургия. Исповедь. 
-						Молебен с Акафистом Донской иконе Божьей Матери и водоосвящнением.
-					Заупокойная панихида.</h6></p>
-					<p style="color: #54749C;">служащий священник:</p>
-					<p style="color: #7030A0;">сослужащий(-е) священник(-и):</p>
-					<p style="color: #943634;">помогающий священник:</p> 
-					<p style="color: #35A907;">отдыхающий священник:</p>
-				</td>
-			</tr>
-			<tr>
-				<td style="color: red;">17:00</td>
-				<td>
-					<p><h6>Вечернее Богослужение. Исповедь. Полиелей свт. Алексию, митр. Киевскому, Московскому и всея России чудотворцу
-					Заупокойная лития.</p></h6>
-					<p style="color: #54749C;">служащий священник:</p>
-					<p style="color: #7030A0;">сослужащий(-е) священник(-и):</p>
-					<p style="color: #943634;">помогающий священник:</p> 
-					<p style="color: #35A907;">отдыхающий священник:</p>
-				</td>
-			</tr>
-		</table>
-		<div class="footer text-center">Ставрополь 2019</div>
-	</div>
-</page>
+					<td style="color: red;"><h3>7:30</h3></td>
 
-	<page size="A4" class="A4"></page>
-	<page size="A4" class="A4"></page>
-	
+					<td>
+						<br>
+						<p><h3 class="bold"><u>Предпразднство Рождества Христова</u><br>
+							Божественная Литургия.
+							Исповедь.
+						Молебен на начало Нового года.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="color: red;"><h3>17:00</h3></td>
+					<td>
+						<p><h3 class="bold">Вечернее Богослужение.
+							Исповедь.
+							Полиелей свт. Петру
+							Московскому, всея России
+						чудотворцу. Заупокойная лития.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+			</table>
+		</div>		
+
+		<!-- ЧЕТВЕРГ 3 ЯНВАРЯ -->
+		<div class="content">
+			<table class="table-r">
+				<tr>
+					<td rowspan="2" class="td-min-width">Четверг<br>3 января</td>
+					<td style="color: red;"><h3>7:30</h3></td>
+
+					<td>
+						<br>
+						<p><h3 class="bold"><u>Предпразднство Рождества Христова</u><br>
+							Божественная Литургия.
+Исповедь.
+Молебен. Заупокойная
+панихида.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="color: red;"><h3>17:00</h3></td>
+					<td>
+						<p><h3 class="bold">
+							Вечернее Богослужение.
+Исповедь.
+Заупокойная лития.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+			</table>
+		</div>		
+	</div>
+</div>
+
+
+<!-- Пятница 4 ЯНВАРЯ -->
+
+<div class="format">
+	<div class="wrap">
+		<div class="A4-border"><img src="image/frame-2487087_960_720.png" alt=""></div>
+		<div class="opacity"><img src="image/thumb_l_0689.jpg" alt=""></div>
+
+		<div class="header">
+			<img src="../images/original-233x285.png" alt="">
+			<div class="title">
+				<h3 class="text-center">
+					БОГОСЛУЖЕНИЯ В ХРАМЕ СВЯТОГО
+					ВЕЛИКОМУЧЕНИКА И ЦЕЛИТЕЛЯ
+					ПАНТЕЛЕИМОНА
+				</h3>
+				<h4 class="text-center">http://stav-panteleimon.ru</h4>
+			</div>
+		</div>
+		<!-- СРЕДА 2 ЯНВАРЯ -->
+		<div class="content">
+			<table class="table-r">
+				<tr>
+					<td rowspan="2" class="td-min-width">Пятница<br>4 января</td>
+					<td style="color: red;"><h3>7:30</h3></td>
+
+					<td>
+						<br>
+						<p><h3 class="bold"><u>Предпразднство Рождества Христова. Св. вмч. Анастасии
+Узорешительницы</u><br>
+							Божественная Литургия.
+							Исповедь.
+						Молебен на начало Нового года.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="color: red;"><h3>17:00</h3></td>
+					<td>
+						<p><h3 class="bold">Вечернее Богослужение.
+							Исповедь.
+							Полиелей свт. Петру
+							Московскому, всея России
+						чудотворцу. Заупокойная лития.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+			</table>
+		</div>		
+
+		<!-- ЧЕТВЕРГ 3 ЯНВАРЯ -->
+		<div class="content">
+			<table class="table-r">
+				<tr>
+					<td rowspan="2" class="td-min-width">Четверг<br>3 января</td>
+					<td style="color: red;"><h3>7:30</h3></td>
+
+					<td>
+						<br>
+						<p><h3 class="bold"><u>Предпразднство Рождества Христова</u><br>
+							Божественная Литургия.
+Исповедь.
+Молебен. Заупокойная
+панихида.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+				<tr>
+					<td style="color: red;"><h3>17:00</h3></td>
+					<td>
+						<p><h3 class="bold">
+							Вечернее Богослужение.
+Исповедь.
+Заупокойная лития.</h3></p><hr>
+						<h3><p style="color: #54749C;">служащий священник:</p></h3>
+						<h3><p style="color: #7030A0;">сослужащий(-е) священник(-и):</p></h3>
+						<h3><p style="color: #943634;">помогающий священник:</p></h3> 
+						<h3><p style="color: #35A907;">отдыхающий священник:</p></h3><br>
+					</td>
+				</tr>
+
+			</table>
+		</div>		
+	</div>
+<	
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
