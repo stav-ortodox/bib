3290d57319b3a7335f28dd7b4059ea2a30221130
Stanislav Perehodchenko <45417701+stav-ortodox@users.noreply.github.com>
2019-04-14 00:56:21 +0300

    сделал просмотор пдф

--- pages/biblioteka/p_opened_post.php
+++ pages/biblioteka/p_opened_post.php
@@ -1,17 +1,21 @@
 <?php 
-session_start();
 require_once $_SERVER['DOCUMENT_ROOT'].'/scripts/s_connect.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/scripts/s_app_config.php';  
 require_once $_SERVER['DOCUMENT_ROOT'].'/scripts/s_functions.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/scripts/s_biblioteka_functions.php';
+$id = $_GET['id'];
 
-get_header_site ('$post_name', 'Электронная библиотека храма святого Великомученика и Целителя Пантелеимона <br> г. Ставрополь');
+$query = ("SELECT pub_name, pub_file FROM publishing_post WHERE id=$id");
+$result = mysqli_query($link, $query);
+$row = mysqli_fetch_assoc($result);
+$post_name = $row['pub_name'];
+$post_file = $row['pub_file'];
+
+
+
+get_header_site ($post_name, 'Электронная библиотека храма святого Великомученика и Целителя Пантелеимона <br> г. Ставрополь');
 new_menu ();
 // get_sidebar ();
-page_title ('$post_name');
-
-echo "<div class='text-center'><h1>Здесь нужно сделать автоматический просмотр файла pdf</h1></div>";
-echo $post_name;?>
+page_title ($post_name);?>
 <main>
 	<div class="col-sm-0 col-lg-2">
 		<?php get_sidebar (); ?>
@@ -19,14 +23,14 @@ echo $post_name;?>
 	<section class="container content">
 		<?php echo bread() ?>
 		<div class="row">
-
 			<div class="page-header">
 				<h1><small><?php echo $block_description; ?></small></h1>
 			</div>
-			<div class="content">
-				<div class="row">
-					<!-- вызов функции выводы шаблона единицы издания -->
-					<?php template_pp () ?>
+			<div class="content m-auto">
+				
+				<iframe src="<?=PATH.$post_file?>" 
+					style="width: 850px; height: 850px;" frameborder="0">Ваш браузер не поддерживает фреймы</iframe>
+					
 				</div>
 			</div>
 		</div>
